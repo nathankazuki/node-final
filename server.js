10b72c55832173980445dd8a2a4fbded10e7c911
nathankazuki <nma24@my.bcit.ca>
2019-04-16 17:03:57 -0700

    update

--- server.js
+++ server.js
@@ -41,8 +41,19 @@ app.get('/feature1', (request, response) => {
 	});
 });
 
+// FEATURE 1 NOT IMPLEMENTED YET
 app.post('/feature1', urlencodedParser, (request, response) => {
-
+	getTemperature(request.body.city).then((message) => {
+		response.render('feature1.hbs', {
+			title: 'Feature 1',
+			output: message
+		});
+	}).catch((error) => {
+		response.render('feature1.hbs', {
+			title: 'Feature 1',
+			output: error
+		});
+	});
 });
 
 app.get('/feature2', (request, response) => {
@@ -51,6 +62,7 @@ app.get('/feature2', (request, response) => {
 	});
 });
 
+// FEATURE 2 NOT IMPLEMENTED YET
 app.post('/feature2', urlencodedParser, (request, response) => {
 
 });
@@ -58,3 +70,10 @@ app.post('/feature2', urlencodedParser, (request, response) => {
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
+
+const getTemperature = async (city) => {
+	const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=7a2a58dca43fc0d58ba404dc3e646a7d`);
+	const temperature = response.data.main.temp - 273.15;
+	const weather = response.data.weather[0].description;
+	return `The current temperature in ${city} is ${temperature.toFixed(1)} degrees Celsius with ${weather}`;
+}
